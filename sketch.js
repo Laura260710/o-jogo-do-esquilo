002c72c1b0586b3e40702bdb871aaaa5384a935c
Laura260710 <110499260+Laura260710@users.noreply.github.com>
2024-02-03 11:52:04 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+let imgjogador, imgobstaculo, imgfundo, imgnuvem;
+let obstaculo = [];
+let nuvem = [];
+let fundo = [];
+let jogador;
+
+function preload(){
+  imgjogador = loadImage("imagens/jogador.png");
+  imgobstaculo = loadImage("imagens/obstaculo.png");
+  imgfundo = loadImage("imagens/fundo.png");
+  imgnuvem = loadImage("imagens/nuvem.png");
+}
+
+function setup() {
+  createCanvas(wi2dth/2, windowHeight/2);
+  jogador = new Jogador();
+}
+
+function draw() {
+  background(220);
+  jogador.show();
+}
