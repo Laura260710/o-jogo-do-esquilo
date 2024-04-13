13469d93a3bf4b5079257346a07b16ec866eeaf8
Laura260710 <110499260+Laura260710@users.noreply.github.com>
2024-04-13 11:04:11 -0300

    coloquei a funcao do game over

--- objetos/jogador.js
+++ objetos/jogador.js
@@ -1,26 +1,34 @@
-class Jogador{
-  constructor(){
-   this.h = 100;
+class Jogador {
+  constructor() {
+    this.h = 100;
     this.w = 80;
     this.x = 50
     this.y = height - this.h;
     this.g = 2.5;
     this.vy = 0;
   }
-  
-  show(){
+
+  show() {
     image(imgjogador, this.x, this.y, this.w, this.h);
   }
 
-  jump(){
-    if(this.y==height - this.h) {
-    this.vy= -35;
-  }
+  jump() {
+    if (this.y == height - this.h) {
+      this.vy = -35;
+    }
   }
 
-  move(){
+  move() {
     this.y += this.vy;
-    this.vy +=this.g;
+    this.vy += this.g;
     this.y = constrain(this.y, 30, height - this.h);
+  }
+
+  hits(obstaculo) {
+    let x1 = this.x + this.w * 0.5;
+    let y1 = this.y + this.h * 0.5;
+    let x2 = obstaculo.x - obstaculo.w * 0.5;
+    let y2 = obstaculo.y - obstaculo.h * 0.5;
+    return collideRectRect(x1, y1, this.w, this.h, x2, y2, obstaculo.w, obstaculo.h);
   }
 }
--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let nuvem = [];
 let fundo = [];
 let jogador;
 
-function preload(){
+function preload() {
   imgjogador = loadImage("imagens/jogador.png");
   imgobstaculo = loadImage("imagens/obstaculo.png");
   imgfundo = loadImage("imagens/fundo.png");
@@ -12,38 +12,42 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(windowWidth/2, windowHeight/2);
+  createCanvas(windowWidth / 2, windowHeight / 2);
   jogador = new Jogador();
 }
 
 function draw() {
   nascerFundo();
-  for(let f of fundo){
+  for (let f of fundo) {
     f.show();
     f.move();
   }
   jogador.show();
   jogador.move();
   nascerObstaculo();
-  for(let o of obstaculo){
+  for (let o of obstaculo) {
     o.show();
     o.move();
+    if (jogador.hits(o)) {
+      console.log("perdeuuu");
+      noLoop();
+    }
   }
 
 }
 
 function keyPressed() {
-  if (key==" "){
+  if (key == " ") {
     jogador.jump();
   }
 }
 
-function nascerObstaculo(){
-  if(frameCount%80===0)
-  obstaculo.push(new Obstaculo());
+function nascerObstaculo() {
+  if (frameCount % 80 === 0)
+    obstaculo.push(new Obstaculo());
 }
 
-function nascerFundo(){
-  if(frameCount%140===0)
-  fundo.push(new Fundo());
+function nascerFundo() {
+  if (frameCount % 140 === 0)
+    fundo.push(new Fundo());
 }
